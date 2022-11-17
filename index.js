4fe6e4f061ad21907f6b60c3893c977d96c23644
BettyDetroit <caubelnelly@gmail.com>
2022-11-17 15:58:25 +0100

    first poo exo done, index.js and src/Fighter.js modified

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require("./src/Fighter");
+
+const displayScore = (winner, loser) => {
+  console.log(`${winner.name} ${loser.name} ${winner.name} : ${winner.life}`);
+};
+
+// const displayStrike = (fighter) => {
+//     console.log(fighter.name, fighter.life);
+// }
+
+const displayStrike = (enemy, damage) => {
+  console.log(
+    `${enemy.name} a perdu ${damage} vie. Il lui reste ${enemy.life} vie`
+  );
+};
+
+// creation des combattants
+const heracles = new Fighter("Heracles", 20, 6);
+const nemean = new Fighter("Nemean Lion", 11, 13);
+
+console.log(heracles.name, heracles.life);
+console.log(nemean.name, nemean.life);
+
+//  lancer la bataille
+// heracles doit attaquer nemean tant que celui ci est en vie
+//  nemean doit rendre les coups tant qu'heracles est en vie
+//  il doit n'en rester qu'un
+
+let round = 1;
+while (heracles.isAlive() && nemean.isAlive()) {
+  console.log(`Round ${round}`);
+  displayStrike(nemean, heracles.fight(nemean));
+
+  if (nemean.isAlive()) {
+    displayStrike(heracles, nemean.fight(heracles));
+  }
+  round++;
+}
+
+if (heracles.isAlive()) {
+  displayScore(heracles, nemean);
+} else {
+  displayScore(nemean, heracles);
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,37 @@
-/* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+constructor(name, strength, dexterity) {
+this.name = name;
+this.strength = strength;
+this.dexterity = dexterity;
+this.life = MAX_LIFE;
+}
+fight(enemy){
+    // calculer un nombre aléatoire entre 1 et strength fighter ( force du coup)
+    const potentialAttack = this.getRandomInt(this.strength)
+    //  evitement : degat = force du coup - dexterite(enemy)
+    const damage = Math.max(potentialAttack - enemy.dexterity, 0); 
+        //  enleve les degats du nd de vie de l'enemy
+    enemy.life = Math.max(enemy.life - damage, 0);
+    return damage;
+}
+
+getRandomInt(max) {
+return Math.floor(Math.random() * max) + 1;
+}
+
+isAlive() {
+    return this.life > 0; 
+}
+};
+
+
+module.exports = Fighter;
+
+
+
+
+
+
+ 
